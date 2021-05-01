5f353aaff65b6e826f0d78694342fcb221c00268
Arif Ahmad <arifa@successive.com>
2021-05-02 02:57:39 +0530

    First DotNet Console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobilityTrainings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

ca698e0b52fbbf6f6c300987d62b78c3b1997359
Arif Ahmad <arifa@successive.com>
2021-05-03 01:22:11 +0530

    Check commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MobilityTrainings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
-        }
+            Console.WriteLine("Hello World!!!!!!!!!!!!! Check commit");
+        } 
     }
 }
